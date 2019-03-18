62b9153ec94f34806a457da9f2823a1fa0d1afd4
Sachin Dev <hellosachindev@gmail.com>
2019-03-18 11:19:25 +0530

    Initial Commit

---
+++ Mez MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mez MacOS
+//
+//  Created by Sachin Dev on 18/03/19.
+//  Copyright © 2019 Sachin Dev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mez MacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mez MacOS
+//
+//  Created by Sachin Dev on 18/03/19.
+//  Copyright © 2019 Sachin Dev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
